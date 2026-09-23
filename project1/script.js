2e3855ea5706a59854682d05fff9a06abff7e0cc
kayla-e-n <kayla.neesmithpro@gmail.com>
2026-09-23 14:28:00 -0400

    input assignment new js

---
+++ project1/script.js
@@ -0,0 +1,7 @@
+let myElement = document.getElementById ("para")
+myElement.style.color = "blue"
+myElement.style.fontSize = "3rem"
+myElement.style.backgroundColor = "yellow"
+myElement.innerHTML += ", welcome"
+
+
